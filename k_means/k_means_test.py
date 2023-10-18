10b331b7c537d3d2e0af510452a24344dc6046d9
bforbanks <benja020201@live.dk>
2023-10-18 18:17:58 +0200

    k_means first

---
+++ k_means/k_means_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestImageLoaderFunctions(unittest.TestCase):
+    def test_load(self):
+        from k_means import cluster
+
+        # test the add function from edgescore
+        cluster("gates.jpg")
+
+
+if __name__ == "__main__":
+    unittest.main()
