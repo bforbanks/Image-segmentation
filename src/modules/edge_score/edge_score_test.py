a6400184ec03df561e0e5c0378664704727d19d0
bforbanks <benja020201@live.dk>
2023-10-25 16:55:43 +0200

    Added centroid coutn to Edge test

--- src/modules/edge_score/edge_score_test.py
+++ src/modules/edge_score/edge_score_test.py
@@ -14,7 +14,7 @@ class TestEgdeScoreFunctions(unittest.TestCase):
         # +---------+                                                       |
         # shoud return [3, 0]                                               V
         self.assertEqual(
-            bottom_scores(np.array([1, 1, 1, 1, 1, 1, 0, 0, 0]), [3, 3]), [3, 0]
+            bottom_scores(np.array([1, 1, 1, 1, 1, 1, 0, 0, 0]), [3, 3], 2), [3, 0]
         )
         # test for the following image:
         # +---------+
@@ -24,15 +24,16 @@ class TestEgdeScoreFunctions(unittest.TestCase):
         # +---------+
         # shoud return [2, 1, 0, 0] because we have 2 centroid-zeros in the lower row and 1 centroid-one.
         self.assertEqual(
-            bottom_scores(np.array([1, 1, 2, 1, 1, 2, 0, 1, 0]), [3, 3]), [2, 1, 0]
+            bottom_scores(np.array([1, 3, 2, 1, 1, 2, 0, 1, 0]), [3, 3], 4), [2, 1, 0]
         )
 
     def test_not_bottom_scores(self):
         self.assertEqual(
-            not_bottom_scores(np.array([1, 1, 1, 1, 1, 1, 0, 0, 0]), [3, 3]), [0, -5]
+            not_bottom_scores(np.array([1, 1, 1, 1, 1, 1, 0, 0, 0]), [3, 3], 2), [0, -5]
         )
         self.assertEqual(
-            not_bottom_scores(np.array([1, 1, 2, 1, 1, 2, 0, 1, 0]), [3, 3]), [0, 3, 2]
+            not_bottom_scores(np.array([1, 1, 2, 1, 1, 2, 0, 1, 0]), [3, 3], 3),
+            [0, 3, 2],
         )
 
     def test_total_scores(self):
