0a7985f3df6a7c3fc6d04b635de1e22982cae4f7
FacelessToxin <bjorn.hagbarth@gmail.com>
2023-10-25 19:06:18 +0200

    Accidentally deleted and restored changes from Tob

--- src/modules/edge_score/edge_score.py
+++ src/modules/edge_score/edge_score.py
@@ -44,20 +44,22 @@ def not_bottom_scores(pixelvector_to_centroids_index, image_shape, centroid) -> 
         image_shape[0], image_shape[1]
     )
     # TODO: implement this function, use the pixelvector_to_centroids_index_2d
+    
+    not_bottom_score = [0] * centroid # sets all elements of list to 0 based on the amount of centroids
+
+    
     rows, columns = pixelvector_to_centroids_index_2d.shape
     top_row = list(pixelvector_to_centroids_index_2d[0, :])
     left_side = list(pixelvector_to_centroids_index_2d[1:-1, 0])
     right_side = list(pixelvector_to_centroids_index_2d[1:-1, columns - 1])
     not_bottom_array = top_row + left_side + right_side
 
-    not_bottom_score = [0] * centroid
-
     for value in not_bottom_array:
         not_bottom_score[value] -= 1
     
-    # example of return. If centroid 0 has -1 point, and centroid 1 has -3 points, return the following array:
     return not_bottom_score
 
+    # example of return. If centroid 0 have have -1 point, and centroid 1 have -3 points, return the following array:
 
 def total_scores(pixelvector_to_centroids_index, image_shape, centroid) -> list[int]:
     """Will use the two scoring functions, and sum them to get the total score for each
@@ -82,11 +84,12 @@ def total_scores(pixelvector_to_centroids_index, image_shape, centroid) -> list[
     scores = bottom_scores(pixelvector_to_centroids_index, image_shape, centroid)
     deduction = not_bottom_scores(pixelvector_to_centroids_index,image_shape,centroid)
 
-    bottom_score = bottom_scores(pixelvector_to_centroids_index, image_shape, centroid)
-    not_bottom_score = not_bottom_scores(pixelvector_to_centroids_index, image_shape, centroid)
+    # Hereafter a forloop based on the amount of centroids (should be the amount of scores) will change the values in the 'scores'
+    # i.e for each score, remove based on the deduction.
 
-    total_score = np.add(not_bottom_score, bottom_score)
+    for i in range(centroid-1):
+        scores[i] = scores[i] + deduction[i]
 
     # Hereafter return the score, which should be identical to the structure of the previous scoring functions, having relied on them as the only source of information.
     # example of return. If centroid 0 have have 1 total point, and centroid 1 have 2 points, return the following array:
-    return list(total_score)
+    return scores
