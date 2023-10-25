3f10329c38a21a054b5e513a116afedf3835c4c8
tob <tobiasbjerre@icloud.com>
2023-10-25 17:28:23 +0200

    Fixed the test

--- src/modules/edge_score/edge_score_test.py
+++ src/modules/edge_score/edge_score_test.py
@@ -24,7 +24,7 @@ class TestEgdeScoreFunctions(unittest.TestCase):
         # +---------+
         # shoud return [2, 1, 0, 0] because we have 2 centroid-zeros in the lower row and 1 centroid-one.
         self.assertEqual(
-            bottom_scores(np.array([1, 3, 2, 1, 1, 2, 0, 1, 0]), [3, 3], 4), [2, 1, 0]
+            bottom_scores(np.array([1, 3, 2, 1, 1, 2, 0, 1, 0]), [3, 3], 4), [2, 1, 0, 0]
         )
 
     def test_not_bottom_scores(self):
